362972e8294ccb0e449a0db3d03ec2c689043831
Suzuki0916 <hs8179189@gmail.com>
2024-05-29 23:49:02 +0530

    spotlight

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black">
-      <div>
-        <h1>
-          Hello
-        </h1>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
       </div>
     </main>
   );
