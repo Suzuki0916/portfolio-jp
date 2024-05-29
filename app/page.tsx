e508adce6c4265181ba6e33cf36347fbd22c7e86
Suzuki0916 <hs8179189@gmail.com>
2024-05-29 19:10:48 +0530

    starter template

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black">
+      <div>
+        <h1>
+          Hello
+        </h1>
+      </div>
+    </main>
+  );
+}
