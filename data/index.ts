17f00a0aa9365e6dd297aa755645fc2d092d9077
Suzuki0916 <hs8179189@gmail.com>
2024-06-05 20:20:36 +0530

    experience

--- data/index.ts
+++ data/index.ts
@@ -9,6 +9,7 @@ export const gridItems = [
   {
     id: 1,
     title: "I prioritize client collaboration, fostering open communication ",
+    
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
